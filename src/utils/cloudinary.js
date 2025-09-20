25a1845e24b09a6294b0aa34c75b0b0c810f3acc
sourav <souravsidana9@gmail.com>
2025-09-20 11:55:55 +0530

    All setup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,54 @@
+// import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+import { v2 as cloudinary } from 'cloudinary';
+
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME, 
+        api_key: process.env.API_KEY, 
+        api_secret: process.env.API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    
+    // Upload an image
+  
+    const uploadToCloudinary = async(localFilePath)=>{
+     try {
+        if(!localFilePath)return null
+        const uploadResult = await cloudinary.uploader
+        .upload(
+            localFilePath, {
+                resource_type : "auto",
+                public_id: 'shoes',
+            }
+        )
+
+     
+     console.log(uploadResult.url);
+     return uploadResult;
+     } catch (error) {
+        console.log(error)
+        fs.unlink(localFilePath)
+        return null
+     }
+    }
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    // const optimizeUrl = cloudinary.url('shoes', {
+    //     fetch_format: 'auto',
+    //     quality: 'auto'
+    // });
+    
+    // console.log(optimizeUrl);
+    
+    // // Transform the image: auto-crop to square aspect_ratio
+    // const autoCropUrl = cloudinary.url('shoes', {
+    //     crop: 'auto',
+    //     gravity: 'auto',
+    //     width: 500,
+    //     height: 500,
+    // });
+    
+    // console.log(autoCropUrl);    
